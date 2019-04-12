e46bc0a992d11536b236f1eaf8025aa667244742
Your Name <lucianzhong@gmail.com>
2019-04-12 14:09:39 +0800

    2019-04-12

--- David_Silver_ RL_course/Actor-Critic.py
+++ David_Silver_ RL_course/Actor-Critic.py
@@ -1,3 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+Actor Critic 方法:
+结合了 Policy Gradient (Actor) 和 Function Approximation (Critic) 的方法. 
+Actor 基于概率选行为, Critic 基于 Actor 的行为评判行为的得分, Actor 根据 Critic 的评分修改选行为的概率
+Actor Critic 方法的优势: 可以进行单步更新, 比传统的 Policy Gradient 要快
+Actor Critic 方法的劣势: 取决于 Critic 的价值判断, 但是 Critic 难收敛, 再加上 Actor 的更新, 就更难收敛
+为了解决收敛问题, Google Deepmind 提出了 Actor Critic 升级版 Deep Deterministic Policy Gradient. 
+后者融合了 DQN 的优势, 解决了收敛难的问题. 我们之后也会要讲到 Deep Deterministic Policy Gradient. 
+不过那个是要以 Actor Critic 为基础
+
+"""
 import numpy as np
 import tensorflow as tf
 import gym
@@ -22,7 +36,7 @@ env = env.unwrapped
 N_F = env.observation_space.shape[0]
 N_A = env.action_space.n
 
-
+# 用 tensorflow 建立 Actor 神经网络
 class Actor(object):
     def __init__(self, sess, n_features, n_actions, lr=0.001):
         self.sess = sess
@@ -50,25 +64,28 @@ class Actor(object):
                 name='acts_prob'
             )
 
+        # Actor 想要最大化期望的 reward, 在 Actor Critic 算法中, 我们用 “比平时好多少” (TD error) 来当做 reward
         with tf.variable_scope('exp_v'):
-            log_prob = tf.log(self.acts_prob[0, self.a])
-            self.exp_v = tf.reduce_mean(log_prob * self.td_error)  # advantage (TD_error) guided loss
-
+            log_prob = tf.log(self.acts_prob[0, self.a])            # log 动作概率
+            self.exp_v = tf.reduce_mean(log_prob * self.td_error)  #  # log 概率 * TD 方向, advantage (TD_error) guided loss
+        # 因为我们想不断增加这个 exp_v (动作带来的额外价值),所以我们用过 minimize(-exp_v) 的方式达到maximize(exp_v) 的目的
         with tf.variable_scope('train'):
             self.train_op = tf.train.AdamOptimizer(lr).minimize(-self.exp_v)  # minimize(-exp_v) = maximize(exp_v)
 
+    # s, a 用于产生 Gradient ascent 的方向,td 来自 Critic, 用于告诉 Actor 这方向对不对
     def learn(self, s, a, td):
         s = s[np.newaxis, :]
         feed_dict = {self.s: s, self.a: a, self.td_error: td}
         _, exp_v = self.sess.run([self.train_op, self.exp_v], feed_dict)
         return exp_v
 
+    #  根据 s 选 行为 a
     def choose_action(self, s):
         s = s[np.newaxis, :]
         probs = self.sess.run(self.acts_prob, {self.s: s})   # get probabilities for all actions
         return np.random.choice(np.arange(probs.shape[1]), p=probs.ravel())   # return a int
 
-
+# 用 tensorflow 建立 Critic 神经网络
 class Critic(object):
     def __init__(self, sess, n_features, lr=0.01):
         self.sess = sess
@@ -98,18 +115,21 @@ class Critic(object):
                 name='V'
             )
 
+        # Critic 的更新很简单, 就是像 Q learning 那样更新现实和估计的误差 (TD error) 就好了.
         with tf.variable_scope('squared_TD_error'):
             self.td_error = self.r + GAMMA * self.v_ - self.v
             self.loss = tf.square(self.td_error)    # TD_error = (r+gamma*V_next) - V_eval
         with tf.variable_scope('train'):
             self.train_op = tf.train.AdamOptimizer(lr).minimize(self.loss)
 
+    # 学习 状态的价值 (state value), 不是行为的价值 (action value),
+    # 计算 TD_error = (r + v_) - v,
+    # 用 TD_error 评判这一步的行为有没有带来比平时更好的结果,
+    # 可以把它看做 Advantage
     def learn(self, s, r, s_):
         s, s_ = s[np.newaxis, :], s_[np.newaxis, :]
-
         v_ = self.sess.run(self.v, {self.s: s_})
-        td_error, _ = self.sess.run([self.td_error, self.train_op],
-                                          {self.s: s, self.v_: v_, self.r: r})
+        td_error, _ = self.sess.run([self.td_error, self.train_op],{self.s: s, self.v_: v_, self.r: r})
         return td_error
 
 
@@ -123,30 +143,25 @@ sess.run(tf.global_variables_initializer())
 if OUTPUT_GRAPH:
     tf.summary.FileWriter("logs/", sess.graph)
 
+#每回合算法
 for i_episode in range(MAX_EPISODE):
     s = env.reset()
     t = 0
-    track_r = []
+    track_r = []  # 每回合的所有奖励
     while True:
         if RENDER: env.render()
-
         a = actor.choose_action(s)
-
         s_, r, done, info = env.step(a)
-
-        if done: r = -20
-
+        if done: r = -20  	 # 回合结束的惩罚
         track_r.append(r)
-
-        td_error = critic.learn(s, r, s_)  # gradient = grad[r + gamma * V(s_) - V(s)]
-        actor.learn(s, a, td_error)     # true_gradient = grad[logPi(s,a) * td_error]
+        td_error = critic.learn(s, r, s_)  #Critic 学习,gradient = grad[r + gamma * V(s_) - V(s)]
+        actor.learn(s, a, td_error)     # Actor 学习,true_gradient = grad[logPi(s,a) * td_error]
 
         s = s_
         t += 1
 
-        if done or t >= MAX_EP_STEPS:
-            ep_rs_sum = sum(track_r)
-
+        if done or t >= MAX_EP_STEPS:  # 回合结束, 打印回合累积奖励
+            ep_rs_sum = sum(track_r)  
             if 'running_reward' not in globals():
                 running_reward = ep_rs_sum
             else:
